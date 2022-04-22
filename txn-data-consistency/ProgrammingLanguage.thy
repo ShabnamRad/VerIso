a13bccde88888730a837d4a0d4827217e1b4fd87
Shabnam Ghasemirad <sghasemirad@student.ethz.ch>
2022-04-22 23:18:35 +0200

    resources + progress + snapshot changed from map to function

--- txn-data-consistency/ProgrammingLanguage.thy
+++ txn-data-consistency/ProgrammingLanguage.thy
@@ -7,10 +7,10 @@ begin
 subsection \<open>Syntax\<close>
 
 datatype cmd_p = Assign vname aexp | Assume aexp
-datatype txn_p = cmd_p | Lookup vname aexp | Mutate aexp aexp
-datatype txn = TSkip | TSeq txn txn (infixr ";" 60) |
+datatype txn_p = TCp cmd_p | Lookup vname aexp | Mutate aexp aexp
+datatype txn = TSkip | Tp txn_p | TSeq txn txn (infixr ";" 60) |
                TChoice txn txn (infixr "[+]" 65) | TItr txn
-datatype cmd = Skip | Atomic txn | Seq cmd cmd (infixr ";;" 60) |
+datatype cmd = Skip | Cp cmd_p | Atomic txn | Seq cmd cmd (infixr ";;" 60) |
                Choice cmd cmd (infixr "[[+]]" 65) | Itr cmd
 
 type_synonym cl_session = nat
@@ -18,8 +18,8 @@ typedecl cl_id
 datatype txid0 = Tn_cl cl_session cl_id
 datatype txid = T0 | txid0
 
-type_synonym key = string
-typedecl val
+type_synonym key = int \<comment>\<open>Should it stay like this? It's implied by the ECOOP paper\<close>
+type_synonym val = int
 consts val0 :: val
 
 record version =
@@ -49,32 +49,53 @@ definition conf_init :: "cl_id \<rightharpoonup> views" where
 definition config_init :: config where
   "config_init \<equiv> \<lparr>c_kvs = kvs_init, c_conf = conf_init\<rparr>"
 
-type_synonym snapshot = "key \<rightharpoonup> version"
+type_synonym snapshot = "key \<Rightarrow> val"
+\<comment>\<open>Should we return version0 for unknown keys?\<close>
+definition last_version :: "kv_store \<Rightarrow> views \<Rightarrow> key \<Rightarrow> version" where
+  "last_version kvs u k \<equiv> (case kvs k (Max (u k)) of None \<Rightarrow> version_init | Some ver \<Rightarrow> ver)"
+
 definition view_snapshot :: "kv_store \<Rightarrow> views \<Rightarrow> snapshot" where
-  "view_snapshot kvs u k \<equiv> kvs k (Max (u k))"
+  "view_snapshot kvs u k \<equiv> v_value (last_version kvs u k)"
+
 definition txn_snapshot :: "config \<Rightarrow> cl_id \<rightharpoonup> snapshot" where
   "txn_snapshot cfg cl \<equiv>
         (case ((c_conf cfg) cl) of
            None \<Rightarrow> None |
-           Some u \<Rightarrow> Some (\<lambda>k. ((c_kvs cfg) k (Max (u k)))))"
+           Some u \<Rightarrow> Some (\<lambda>k. v_value (last_version (c_kvs cfg) u k)))"
 
 datatype op_type = R | W
+datatype op = Read key val | Write key val | Eps
 
-record fp_elem =
-  f_op :: op_type
-  f_key :: key
-  f_val :: val
+type_synonym fingerprint = "key \<times> op_type \<rightharpoonup> val"
 
-type_synonym fingerprint = "key \<rightharpoonup> fp_elem"
-
-fun update_fp :: "fingerprint \<Rightarrow> op_type \<Rightarrow> key \<Rightarrow> val \<Rightarrow> fingerprint" where
-  "update_fp fp R k v = (if fp k = None
-                          then fp (k := Some \<lparr>f_op=R, f_key=k, f_val=v\<rparr>)
-                          else fp)" |
-  "update_fp fp W k v = fp (k := Some \<lparr>f_op=W, f_key=k, f_val=v\<rparr>)"
+fun update_fp :: "fingerprint \<Rightarrow> op \<Rightarrow> fingerprint" where
+  "update_fp fp (Read k v)  = (case fp (k, R) of
+                                None \<Rightarrow> (case fp (k, W) of
+                                          None \<Rightarrow> fp ((k, R) := Some v) |
+                                          Some _ \<Rightarrow> fp) |
+                                Some _ \<Rightarrow> fp)" |
+  "update_fp fp (Write k v) = fp ((k, W) := Some v)" |
+  "update_fp fp Eps         = fp"
 
 type_synonym stack = state
-fun t_primitive :: "stack \<Rightarrow> snapshot \<Rightarrow> txn_p \<Rightarrow> stack \<Rightarrow> snapshot" where
-  "t_primitive s \<sigma> (Assign x E) s' \<sigma>' \<equiv> s' = s(x := aval E s) \<and> \<sigma>' = \<sigma>"
+fun t_primitive :: "stack \<Rightarrow> snapshot \<Rightarrow> txn_p \<Rightarrow> stack \<Rightarrow> snapshot \<Rightarrow> bool" where
+  "t_primitive s \<sigma> (TCp (Assign x E)) s' \<sigma>' \<longleftrightarrow> s' = s(x := aval E s) \<and> \<sigma>' = \<sigma>" |
+  "t_primitive s \<sigma> (TCp (Assume E)) s' \<sigma>'   \<longleftrightarrow> s' = s \<and> \<sigma>' = \<sigma> \<and> aval E s \<noteq> 0" |
+  "t_primitive s \<sigma> (Lookup x E) s' \<sigma>'       \<longleftrightarrow> s' = s(x := \<sigma>(aval E s)) \<and> \<sigma>' = \<sigma>" |
+  "t_primitive s \<sigma> (Mutate E1 E2) s' \<sigma>'     \<longleftrightarrow> s' = s \<and> \<sigma>' = \<sigma>((aval E1 s) := aval E2 s)"
 
+fun get_op :: "stack \<Rightarrow> snapshot \<Rightarrow> txn_p \<Rightarrow> op" where
+  "get_op s \<sigma> (TCp (Assign x E)) = Eps" |
+  "get_op s \<sigma> (TCp (Assume E))   = Eps" |
+  "get_op s \<sigma> (Lookup x E)       = Read (aval E s) (\<sigma>(aval E s))" |
+  "get_op s \<sigma> (Mutate E1 E2)     = Write (aval E1 s) (aval E2 s)"
+
+fun c_primitive :: "stack \<Rightarrow> cmd_p \<Rightarrow> stack \<Rightarrow> bool" where
+  "c_primitive s (Assign x E) s' \<longleftrightarrow> s' = s(x := aval E s)" |
+  "c_primitive s (Assume E) s'   \<longleftrightarrow> s' = s \<and> aval E s \<noteq> 0"
+
+(*fun update_kv :: "kv_store \<Rightarrow> views \<Rightarrow> fingerprint \<Rightarrow> txid \<Rightarrow> kv_store" where
+  "update_kv kvs u fp t k \<equiv> (kvs k)((Max (u k)) := \<lparr>v_value=)" \<comment>\<open>How to append a new version\<close>
+
+fun c_atomic_trans :: "kv_store \<Rightarrow> views \<Rightarrow> stack \<Rightarrow> txn \<Rightarrow> cl_id \<Rightarrow> fingerprint \<Rightarrow> kv_store \<Rightarrow> views \<Rightarrow> stack"*)
 end
