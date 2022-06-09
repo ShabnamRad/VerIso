f4c11483327fea116d851c80d7234f9ccca664f7
Shabnam Ghasemirad <sghasemirad@student.ethz.ch>
2022-06-09 13:55:49 +0200

    unclear what to do about next round

--- txn-data-consistency/Prot_2PC_2PL.thy
+++ txn-data-consistency/Prot_2PC_2PL.thy
@@ -8,7 +8,7 @@ subsection \<open>2PC Event system\<close>
 
 subsubsection \<open>State\<close>
 
-datatype status_rm = working | prepared | read_lock | write_lock | notokay | committed | aborted
+datatype status_rm = working | prepared | read_lock | write_lock | no_lock | notokay | committed | aborted
 datatype status_tm = tm_init | tm_prepared | tm_committed | tm_aborted
 
 record tm_state =
@@ -29,7 +29,7 @@ record 'v global_state =
 subsubsection \<open>Events\<close>
 
 datatype 'v rm_ev = Write2 txid0 'v | Read2 txid0 'v | Prepare txid0 | RLock txid0 | WLock txid0 |
-                 NOK txid0 | Commit txid0 | Abort txid0 | Ready txid0
+                 NoLock txid0 | NOK txid0 | Commit txid0 | Abort txid0
 datatype tm_ev = User_Commit | TM_Commit | TM_Abort | TM_ReadyC | TM_ReadyA | TM_Skip
 datatype 'v ev = RM_ev key "'v rm_ev" | TM_ev cl_id tm_ev
 
@@ -42,7 +42,7 @@ definition other_rms_unchanged where
   "other_rms_unchanged k rmss rmss' \<equiv> (\<forall>k'. k' \<noteq> k \<longrightarrow> rmss' k' = rmss k')"
 
 abbreviation tm_other_rms_unchanged where
-  "tm_other_rms_unchanged t k s s' \<equiv> tm s' = tm s \<and>
+  "tm_other_rms_unchanged k s s' t \<equiv> tm s' = tm s \<and>
     other_rms_unchanged k (rms s) (rms s') \<and> other_instances_unchanged t k (rms s) (rms s')"
 
 abbreviation rm_status_trans where
@@ -51,7 +51,7 @@ abbreviation rm_status_trans where
     \<and> rm_db (rms s' k) = rm_db (rms s k)
     \<and> rm_fingerpr (rms s' k) = rm_fingerpr (rms s k)
     \<and> rm_status (rms s' k) t = to_stat
-    \<and> tm_other_rms_unchanged t k s s'"
+    \<and> tm_other_rms_unchanged k s s' t"
 
 fun tid_inc :: "txid0 \<Rightarrow> txid0" where
   "tid_inc (Tn_cl sn cl) = Tn_cl (Suc sn) cl"
@@ -62,7 +62,7 @@ definition write2 where
                         \<and> rm_fingerpr (rms s' k) t = update_key_fp (rm_fingerpr (rms s k) t) W v
                         \<and> rm_status (rms s' k) t = working
                         \<and> rm_db (rms s' k) = rm_db (rms s k)
-                        \<and> tm_other_rms_unchanged t k s s'"
+                        \<and> tm_other_rms_unchanged k s s' t"
 
 definition read2 where
   "read2 s v k s' cl  \<equiv> let t = (Tn_cl (tm_sn (tm s cl)) cl) in
@@ -70,7 +70,7 @@ definition read2 where
                         \<and> rm_fingerpr (rms s' k) t = update_key_fp (rm_fingerpr (rms s k) t) R v
                         \<and> rm_status (rms s' k) t = working
                         \<and> rm_db (rms s' k) = rm_db (rms s k)
-                        \<and> tm_other_rms_unchanged t k s s'"
+                        \<and> tm_other_rms_unchanged k s s' t"
 
 definition prepare where
   "prepare s k s' cl \<equiv> tm_status (tm s cl) = tm_prepared
@@ -80,88 +80,87 @@ definition acquire_rd_lock where \<comment>\<open>Read Lock acquired\<close>
   "acquire_rd_lock s k s' cl \<equiv> let t = (Tn_cl (tm_sn (tm s cl)) cl) in 
                    tm_status (tm s cl) = tm_prepared
                   \<and> rm_fingerpr (rms s k) t W = None
-                  \<and> (rm_fingerpr (rms s k) t R = None \<or>
-                      (rm_fingerpr (rms s k) t R \<noteq> None \<and>
-                      (\<forall>t'. rm_status (rms s k) t' \<in> {working, prepared, read_lock, notokay})))
+                  \<and> rm_fingerpr (rms s k) t R \<noteq> None
+                  \<and> (\<forall>t'. rm_status (rms s k) t' \<in> {working, prepared, read_lock, no_lock, notokay})
                   \<and> rm_status_trans s k s' t prepared read_lock"
 
-definition acquire_rd_lock where \<comment>\<open>Write Lock acquired\<close>
-  "acquire_rd_lock s k s' cl \<equiv> let t = (Tn_cl (tm_sn (tm s cl)) cl) in 
+definition acquire_wr_lock where \<comment>\<open>Write Lock acquired\<close>
+  "acquire_wr_lock s k s' cl \<equiv> let t = (Tn_cl (tm_sn (tm s cl)) cl) in 
+                   tm_status (tm s cl) = tm_prepared
+                  \<and> rm_fingerpr (rms s k) t W \<noteq> None
+                  \<and> (\<forall>t'. rm_status (rms s k) t' \<in> {working, prepared, no_lock, notokay})
+                  \<and> rm_status_trans s k s' t prepared write_lock"
+
+definition acquire_no_lock where \<comment>\<open>No Lock needed\<close>
+  "acquire_no_lock s k s' cl \<equiv> let t = (Tn_cl (tm_sn (tm s cl)) cl) in 
                    tm_status (tm s cl) = tm_prepared
                   \<and> rm_fingerpr (rms s k) t W = None
-                  \<and> rm_status_trans s k s' t prepared read_lock"
+                  \<and> rm_fingerpr (rms s k) t R = None
+                  \<and> rm_status_trans s k s' t prepared no_lock"
 
 definition nok where \<comment>\<open>Lock not available\<close>
-  "nok s rm s' \<equiv> tm_status (tm s) = tm_prepared
-                \<and> rm_status_trans s rm s' prepared notokay"
+  "nok s k s' cl \<equiv> let t = (Tn_cl (tm_sn (tm s cl)) cl) in 
+                    tm_status (tm s cl) = tm_prepared \<and>
+                    ((rm_fingerpr (rms s k) t R \<noteq> None \<and>
+                      (\<exists>t'. rm_status (rms s k) t' = write_lock)) \<or>
+                     (rm_fingerpr (rms s k) t W \<noteq> None \<and>
+                      (\<exists>t'. rm_status (rms s k) t' \<in> {write_lock, read_lock}))) \<and>
+                    rm_status_trans s k s' t prepared notokay"
 
 definition commit where
-  "commit s rm s' \<equiv> rm_status (rms s k) = okay
-                    \<and> tm_status (tm s) = tm_committed
-                    \<and> rm_db (rms s' k) =  update_kv (rm_tid (rms s k)) (rm_fingerpr (rms s k))
-                                                     (tm_view (tm s)) (rm_db (rms s k)) \<comment>\<open>?\<close>
-                    \<and> rm_fingerpr (rms s' k) = Map.empty
-                    \<and> rm_status (rms s' k) = committed
-                    \<and> rm_tid (rms s' k) = rm_tid (rms s k)
-                    \<and> tm_other_rms_unchanged rm s s'"
+  "commit s k s' cl \<equiv> let t = (Tn_cl (tm_sn (tm s cl)) cl) in 
+                        rm_status (rms s k) t \<in> {read_lock, write_lock, no_lock}
+                      \<and> tm_status (tm s cl) = tm_committed
+                      \<and> rm_db (rms s' k) =  update_kv t (rm_fingerpr (rms s k) t)
+                                                       (tm_view (tm s)) (rm_db (rms s k)) \<comment>\<open>?\<close>
+                      \<and> rm_fingerpr (rms s' k) t = Map.empty
+                      \<and> rm_status (rms s' k) t = committed
+                      \<and> tm_other_rms_unchanged rm s s' t"
 
-definition abort where \<comment>\<open>Lock released (if starting from okay)\<close>
-  "abort s rm s' \<equiv> (rm_status (rms s k) = okay \<or>
-                    rm_status (rms s k) = notokay)
-                    \<and> tm_status (tm s) = tm_aborted
+definition abort where \<comment>\<open>Locks released (aborted)\<close>
+  "abort s k s' cl \<equiv> let t = (Tn_cl (tm_sn (tm s cl)) cl) in 
+                      rm_status (rms s k) t \<in> {read_lock, write_lock, no_lock, notokay}
+                    \<and> tm_status (tm s cl) = tm_aborted
                     \<and> rm_db (rms s' k) = rm_db (rms s k)
-                    \<and> rm_fingerpr (rms s' k) = Map.empty
-                    \<and> rm_status (rms s' k) = aborted
-                    \<and> rm_tid (rms s' k) = rm_tid (rms s k)
-                    \<and> tm_other_rms_unchanged rm s s'"
-
-definition ready where \<comment>\<open>Lock released (if starting from committed)\<close>
-  "ready s rm s' \<equiv> ((rm_status (rms s k) = committed \<and> tm_status (tm s) = tm_committed)
-                      \<or> (rm_status (rms s k) = aborted \<and> tm_status (tm s) = tm_aborted))
-                   \<and> rm_status (rms s' k) = working
-                   \<and> rm_tid (rms s' k) = tid_inc (rm_tid (rms s k))
-                   \<and> rm_db (rms s' k) = rm_db (rms s k)
-                   \<and> rm_fingerpr (rms s' k) = rm_fingerpr (rms s k)
-                   \<and> tm_other_rms_unchanged rm s s'"
+                    \<and> rm_fingerpr (rms s' k) t = Map.empty
+                    \<and> rm_status (rms s' k) t = aborted
+                    \<and> tm_other_rms_unchanged k s s' t"
 
 definition user_commit where
-  "user_commit s s' \<equiv> tm_status (tm s) = tm_init
-                      \<and> tm_status (tm s') = tm_prepared
-                      \<and> tm_tid (tm s') = tm_tid (tm s)
-                      \<and> tm_view (tm s') = tm_view (tm s)
+  "user_commit s s' cl \<equiv> tm_status (tm s cl) = tm_init
+                      \<and> tm_status (tm s' cl) = tm_prepared
+                      \<and> tm_sn (tm s' cl) = tm_sn (tm s cl)
                       \<and> rms s' = rms s"
 
 definition tm_commit where
-  "tm_commit s s' \<equiv> tm_status (tm s) = tm_prepared
-                    \<and> (\<forall>rm. rm_status (rms s k) = okay)
-                    \<and> tm_status (tm s') = tm_committed
-                    \<and> tm_tid (tm s') = tm_tid (tm s)
-                    \<and> tm_view (tm s') = tm_view (tm s)
+  "tm_commit s s' cl \<equiv> let t = (Tn_cl (tm_sn (tm s cl)) cl) in
+                        tm_status (tm s cl) = tm_prepared
+                    \<and> (\<forall>k. rm_status (rms s k) t \<in> {read_lock, write_lock, no_lock})
+                    \<and> tm_status (tm s' cl) = tm_committed
+                    \<and> tm_sn (tm s' cl) = tm_sn (tm s cl)
                     \<and> rms s' = rms s"
 
 definition tm_abort where
-  "tm_abort s s' \<equiv> (tm_status (tm s) = tm_prepared
-                     \<and> (\<exists>rm. rm_status (rms s k) = notokay)
-                     \<and> (\<forall>rm. rm_status (rms s k) = okay \<or> rm_status (rms s k) = notokay))
-                    \<and> tm_status (tm s') = tm_aborted
-                    \<and> tm_tid (tm s') = tm_tid (tm s)
-                    \<and> tm_view (tm s') = tm_view (tm s)
+  "tm_abort s s' cl \<equiv> let t = (Tn_cl (tm_sn (tm s cl)) cl) in
+                     (tm_status (tm s cl) = tm_prepared
+                     \<and> (\<exists>k. rm_status (rms s k) t = notokay)
+                     \<and> (\<forall>k. rm_status (rms s k) t \<in> {read_lock, write_lock, no_lock, notokay}))
+                    \<and> tm_status (tm s' cl) = tm_aborted
+                    \<and> tm_sn (tm s' cl) = tm_sn (tm s cl)
                     \<and> rms s' = rms s"
 
 definition tm_ready_c where
-  "tm_ready_c s s' \<equiv> tm_status (tm s) = tm_committed
-                    \<and> (\<forall>rm. rm_status (rms s k) = working \<and> rm_tid (rms s k) = tid_inc (tm_tid (tm s)))
-                    \<and> tm_status (tm s') = tm_init
-                    \<and> tm_tid (tm s') = tid_inc (tm_tid (tm s))
-                    \<and> tm_view (tm s') = tm_view (tm s) \<comment>\<open>Should see its own transaction?\<close>
+  "tm_ready_c s s' \<equiv> tm_status (tm s cl) = tm_committed
+                    \<and> (\<forall>k. rm_status (rms s k) = working \<and> rm_tid (rms s k) = tid_inc (tm_tid (tm s)))
+                    \<and> tm_status (tm s' cl) = tm_init
+                    \<and> tm_sn (tm s' cl) = Suc (tm_sn (tm s cl))
                     \<and> rms s' = rms s"
 
 definition tm_ready_a where
-  "tm_ready_a s s' \<equiv> tm_status (tm s) = tm_aborted
-                    \<and> (\<forall>rm. rm_status (rms s k) = working \<and> rm_tid (rms s k) = tid_inc (tm_tid (tm s)))
-                    \<and> tm_status (tm s') = tm_init
-                    \<and> tm_tid (tm s') = tid_inc (tm_tid (tm s))
-                    \<and> tm_view (tm s') = tm_view (tm s) \<comment>\<open>Should see its own transaction?\<close>
+  "tm_ready_a s s' \<equiv> tm_status (tm s cl) = tm_aborted
+                    \<and> (\<forall>k. rm_status (rms s k) = working \<and> rm_tid (rms s k) = tid_inc (tm_tid (tm s)))
+                    \<and> tm_status (tm s' cl) = tm_init
+                    \<and> tm_sn (tm s' cl) = Suc (tm_sn (tm s cl))
                     \<and> rms s' = rms s"
 
 consts cl0 :: cl_id
