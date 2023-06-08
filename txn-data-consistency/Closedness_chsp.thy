5642152f11f1be8d513f8a2a224720adb31e5b2f
Christoph Sprenger <sprenger@inf.ethz.ch>
2023-06-08 13:58:23 +0200

    further generalized union lemma and added some new lemmas

--- txn-data-consistency/Closedness_chsp.thy
+++ txn-data-consistency/Closedness_chsp.thy
@@ -37,6 +37,13 @@ obviates need for some assumptions in lemmas below; also somewhat easier to work
 definition closed_general :: "'a set \<Rightarrow> 'a rel \<Rightarrow> 'a set \<Rightarrow> bool" where
   "closed_general V r N \<longleftrightarrow> (r\<^sup>*) `` V \<subseteq> V \<union> N"
 
+lemma closed_generalI: "(r\<^sup>*) `` V \<subseteq> V \<union> N \<Longrightarrow> closed_general V r N"
+  by (simp add: closed_general_def)
+
+lemma closed_generalE [elim]: 
+  "\<lbrakk> closed_general V r N; (r\<^sup>*) `` V \<subseteq> V \<union> N \<Longrightarrow> P \<rbrakk> \<Longrightarrow> P"
+  by (simp add: closed_general_def)
+
 lemma closed_general_equiv:
   assumes "V \<inter> N = {}"
   shows "closed_general V r N \<longleftrightarrow> closed_general' V r N"
@@ -44,39 +51,35 @@ lemma closed_general_equiv:
   by (unfold closed_general_def closed_general'_def) blast    \<comment> \<open>just by general set theory\<close>
 
 
-text \<open>Extending N\<close>
+text \<open>Empty set, monotonicity, and unions\<close>
 
-lemma closed_general_mono_N:
+lemma closed_general_empty [simp, intro!]: 
+  shows "closed_general {} r N"
+  by (auto simp add: closed_general_def)
+
+lemma closed_general_mono_N [elim]:
   assumes "closed_general V r N" 
   and "N \<subseteq> N'" (* "N' \<inter> V = {}"; assumption not needed with alternative def *)
   shows "closed_general V r N'"
   using assms
   by (auto simp add: closed_general_def)
 
-
-text \<open>Extending V\<close>
-
-lemma closed_general_mono_V:
+lemma closed_general_mono_V:        \<comment> \<open>does not hold!\<close>
   assumes "closed_general V r N" 
   and "V \<subseteq> V'" 
   shows "closed_general V' r N"
   using assms
   apply (auto simp add: closed_general_def del: subsetI)
-  quickcheck
+  \<comment> \<open>quickcheck finds a counterexample\<close> 
   oops
 
-lemma closed_general_union_V:                \<comment> \<open>just union...\<close>
-  assumes "closed_general V\<^sub>1 r N"
-      and "closed_general V\<^sub>2 r N"
-  shows "closed_general (V\<^sub>1 \<union> V\<^sub>2) r N"
-  using assms
-  by (auto simp add: closed_general_def)
 
-lemma closed_general_insert_V:
-  assumes "closed_general V r N"
-  and "r\<^sup>* `` {t} \<subseteq> insert t (V \<union> N)"
-  (* and "t \<notin> N"; assumption not needed with alternative def! *) 
-  shows "closed_general (insert t V) r N"
+lemma closed_general_set_union_closed:   
+  assumes "closed_general V\<^sub>1 r N\<^sub>1"
+      and "closed_general V\<^sub>2 r N\<^sub>2"
+      and "V = V\<^sub>1 \<union> V\<^sub>2"
+      and "N\<^sub>1 \<union> N\<^sub>2 \<subseteq> V\<^sub>1 \<union> V\<^sub>2 \<union> N"
+  shows "closed_general V r N"
   using assms
   by (auto simp add: closed_general_def)
 
@@ -86,12 +89,12 @@ text \<open>Extending the relation\<close>
 lemma closed_general_insert_rel_max:
   assumes "closed_general V r N" 
   and "r' = insert (x, y) r" 
-  and "x \<notin> (r\<^sup>*) `` V" 
+  and "x \<notin> (r\<^sup>*) `` V"            \<comment> \<open>implies @{prop "x \<notin> V"} and @{prop "x \<notin> r``V"}\<close>
   shows "closed_general V r' N"
   using assms
   by (auto simp add: closed_general_def Image_rtrancl_insert_max)
 
-lemma closed_general_extend_rel_with_max:    \<comment> \<open>generalizes previous lemma\<close>
+lemma closed_general_extend_rel_max:    \<comment> \<open>generalizes previous lemma\<close>
   assumes "closed_general V r N" 
   and "r' = (\<Union>y\<in>Y. {(x, y)}) \<union> r" 
   and "x \<notin> (r\<^sup>*) `` V" 
@@ -150,7 +153,7 @@ lemma closed_general_union_V_extend_N:
       (* and "r_only' \<inter> (vis\<^sub>1 \<union> vis\<^sub>2) = {}"; not needed with alternative def *)
     shows "closed_general (vis\<^sub>1 \<union> vis\<^sub>2) r r_only'"
   using assms
-  by (auto simp add: closed_general_union_V closed_general_mono_N)
+  by (auto intro: closed_general_set_union_closed)
 
 
 \<comment> \<open>all at once ;-)\<close>
@@ -165,12 +168,8 @@ lemma closed_general_union_V_extend_N_extend_rel:
       (* and "V \<subseteq> vis\<^sub>1 \<union> vis\<^sub>2";  assumption not needed  *)
       and "finite V"
     shows "closed_general (vis\<^sub>1 \<union> vis\<^sub>2) r' r_only'"         \<comment> \<open>r changes as well!\<close>
-proof -
-  have "closed_general (vis\<^sub>1 \<union> vis\<^sub>2) r r_only'" using assms(1-3) 
-    by (simp add: closed_general_union_V_extend_N)
-  then show ?thesis using assms(4-6)
-    by (simp add: closed_general_extend_rel_with_max)
-qed
+  using assms
+  by (auto intro: closed_general_set_union_closed[THEN closed_general_extend_rel_max])
 
 
 (****************************************************************************************)
@@ -204,7 +203,7 @@ lemma union_closed':
   shows "closed' K' (u\<^sub>1 \<union> u\<^sub>2) r"
   using assms
   by (auto simp add: closed'_generalize visTx'_union_distr visTx'_same_writers[of K']
-           intro!: closed_general_union_V_extend_N)
+           intro: closed_general_set_union_closed)
 
 
 lemma visTx'_new_writer: "kvs_writers K' = insert t (kvs_writers K) \<Longrightarrow>
@@ -214,13 +213,14 @@ lemma visTx'_new_writer: "kvs_writers K' = insert t (kvs_writers K) \<Longrighta
 lemma insert_wr_t_closed':
   assumes "closed' K u r"
     (* and "t \<notin> visTx' K u";  not needed with alternative def! *)
-    and "(r^-1)\<^sup>* `` {t} \<subseteq> insert t (visTx' K u \<union> read_only_Txs K)"
+    and "closed_general {t} (r^-1) (visTx' K u \<union> read_only_Txs K)"
+    (* and "(r^-1)\<^sup>* `` {t} \<subseteq> insert t (visTx' K u \<union> read_only_Txs K)"; same previous premise *)
     and "read_only_Txs K' = read_only_Txs K"
     and "kvs_writers K' = insert t (kvs_writers K)"
     and "snd ` t_wr_deps = {t}"
   shows "closed' K' (u \<union> t_wr_deps) r"
   using assms
-  by (auto simp add: closed'_generalize visTx'_new_writer intro!: closed_general_insert_V del: subsetI)
+  by (auto simp add: closed'_generalize visTx'_new_writer intro: closed_general_set_union_closed)
 
 \<comment> \<open>insert (k, t) in version's deps - used in get_ctx\<close>
 lemma visTx'_observes_t:
@@ -231,10 +231,12 @@ lemma insert_kt_to_deps_closed':
   assumes "closed' K deps r"
     and "t \<in> kvs_writers K" 
     (* and "t \<notin> visTx' K deps";  not needed with alternative def! *)
-    and "(r^-1)\<^sup>* `` {t} \<subseteq> insert t (visTx' K deps \<union> read_only_Txs K)"
+    and "closed_general {t} (r^-1) (visTx' K deps \<union> read_only_Txs K)"
+    (* and "(r^-1)\<^sup>* `` {t} \<subseteq> insert t (visTx' K deps \<union> read_only_Txs K)"; same as prvious premise *) 
+
   shows "closed' K (insert (k, t) deps) r"
   using assms
-  by (auto simp add: closed'_generalize visTx'_observes_t intro!: closed_general_insert_V)
+  by (auto simp add: closed'_generalize visTx'_observes_t intro: closed_general_set_union_closed)
 
 
 \<comment> \<open>concrete read_done closedness\<close>
